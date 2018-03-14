84a538846c4b15cefaa8f54e14c1469641730ee6
Justin Makaila <justinmakaila@gmail.com>
2018-03-14 11:44:40 -0400

    Added support for merging schemas together and generating types for all.

--- example/annotations.flow.js
+++ example/annotations.flow.js
@@ -1,15 +1,45 @@
 /* @flow */
+/* eslint-disable */
 //  This file was automatically generated and should not be edited.
+
+export type Episode =
+  "EMPIRE" |
+  "JEDI" |
+  "NEWHOPE";
+
+
+export type ReviewInput = {|
+  stars: number,
+  commentary?: ?string,
+  favorite_color?: ?ColorInput,
+|};
+
+export type ColorInput = {|
+  red: number,
+  green: number,
+  blue: number,
+|};
 
 export type HeroQuery = {|
   hero: ?( {
       __typename: "Human",
-      // The name of the character
       name: string,
     } | {
       __typename: "Droid",
-      // The name of the character
       name: string,
     }
   ),
+|};
+
+export type CreateReviewMutationVariables = {|
+  episode?: ?Episode,
+  review: ReviewInput,
+|};
+
+export type CreateReviewMutation = {|
+  createReview: ? {|
+    __typename: "Review",
+    stars: number,
+    commentary: ?string,
+  |},
 |};
--- src/codegen.ts
+++ src/codegen.ts
@@ -1,66 +1,90 @@
-import {
-  generate,
-} from 'apollo-codegen';
-import {
-  buildASTSchema,
-  parse,
-  GraphQLSchema,
-  graphql,
-} from 'graphql';
-import { introspectionQuery } from 'graphql/utilities';
-import * as path from 'path';
-import * as fs from 'fs';
-import * as glob from 'glob';
+import { buildASTSchema, parse, GraphQLSchema, graphql } from "graphql";
+import { generate } from "apollo-codegen";
+import * as path from "path";
+import * as fs from "fs";
+import * as glob from "glob";
+import { introspectionQuery } from "graphql/utilities";
+import { mergeTypes } from "merge-graphql-schemas";
+import { promisify } from "util";
+import { difference } from "lodash";
 
-export const command = 'codegen [--target] [--output]';
-export const desc = 'Generates apollo-codegen code/annotations from your .graphqlconfig';
+const TEMPORARY_SCHEMA_PATH = "/tmp/schema.json";
+
+export const command = "codegen [--target] [--output]";
+export const desc =
+  "Generates apollo-codegen code/annotations from your .graphqlconfig";
 export const builder = {};
 
-const globAsync = (globPath: string): Promise<string[]> => {
-  return new Promise((res, rej) => {
-    glob(globPath, (err, paths) => {
-      if (err) {
-        return rej(err);
-      }
-      res(paths || []);
-    });
-  });
-};
-
-const getSchema = async (config, configPath): Promise<GraphQLSchema> => {
-  const configBase = path.dirname(configPath);
-  const schemaPath = path.join(configBase, config.schemaPath);
-  const schemaContents = fs.readFileSync(schemaPath).toString();
-  const schema = await buildASTSchema(parse(schemaContents));
-  return schema;
-};
-
-const writeSchema = async (schema: GraphQLSchema, path: string): Promise<void> => {
-  const results = await graphql(schema, introspectionQuery);
-  fs.writeFileSync(path, JSON.stringify(results));
-};
-
-const getInputs = async (config, configPath): Promise<string[]> => {
-  const configBase = path.dirname(configPath);
-  const includes: string[] = config.includes || [];
-  const globResults = await Promise.all(includes.map(g => globAsync(g)));
-  return globResults.reduce((acc, r) => [...acc, ...r]);
-};
-
-export const handler = async ({getConfig}) => {
-  const {config, configPath} = getConfig();
-  const options = (config.extensions || {}).codegen || {};
-  const configBase = path.dirname(configPath);
-  const schema = await getSchema(config, configPath);
-  const schemaLoc = '/tmp/gql-schema.json'; // todo: change this
-  await writeSchema(schema, schemaLoc);
-  const inputFiles = await getInputs(config, configPath);
-  generate(
-    inputFiles,
-    schemaLoc,
-    path.join(configBase, options.output || 'codegen'),
-    options.target || 'swift' ,
-    options.tagName || 'gql',
-    options,
+const loadGlob = async (paths: string[] = []): Promise<string[]> => {
+  const normalizedPaths = await Promise.all(
+    paths.map(
+      (_path): Promise<string[]> =>
+        new Promise((resolve, reject) => {
+          glob(_path, (error, paths) => {
+            if (error) {
+              return reject(error);
+            }
+            resolve(paths || []);
+          });
+        })
+    )
   );
+
+  return normalizedPaths.reduce((returnValue, nextValue) => {
+    return [...returnValue, ...nextValue];
+  }, []);
+};
+
+const mergeSchemas = async (schemaPaths: string[]): Promise<GraphQLSchema> => {
+  return await buildASTSchema(
+    parse(
+      mergeTypes(
+        schemaPaths.map(value => fs.readFileSync(value, "utf8").toString())
+      )
+    )
+  );
+};
+
+const writeSchema = async (
+  schema: GraphQLSchema,
+  outputPath: string
+): Promise<void> => {
+  const results = await graphql(schema, introspectionQuery);
+  await promisify(fs.writeFile)(outputPath, JSON.stringify(results));
+};
+
+export const handler = async (context, argv) => {
+  const { project } = argv;
+  const { getProjectConfig } = context;
+  const { config, configPath } = await getProjectConfig(project);
+  const {
+    extensions: { codegen: options = {} } = {},
+    includes = [],
+    excludes = [],
+    schemaPath,
+  } = config;
+  const {
+    target = "swift",
+    tagName = "gql",
+    output = "codegen",
+    mergedSchemaOutputPath = TEMPORARY_SCHEMA_PATH,
+    schemas = [],
+  } = options;
+
+  const outputPath = path.resolve(mergedSchemaOutputPath);
+  const inputFiles = difference(
+    await loadGlob(includes),
+    await loadGlob(excludes)
+  ).map(file => path.resolve(file));
+
+  const schema = await mergeSchemas(await loadGlob(schemas));
+
+  try {
+    await writeSchema(schema, outputPath);
+    generate(inputFiles, outputPath, output, "", target, tagName, project, {
+      addTypename: true,
+    });
+  } catch (error) {
+    console.log("error:", error);
+  }
 };
